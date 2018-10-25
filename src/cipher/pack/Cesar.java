d65b5bd97841b0a46c9842ee6eb3562299578c0f
mardab1994 <mardab1994@gmail.com>
2018-10-25 21:35:24 +0200

    DH

--- src/cipher/pack/Cesar.java
+++ src/cipher/pack/Cesar.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package cipher.pack;
 
 public class Cesar {
@@ -30,36 +30,4 @@ public class Cesar {
 		return cipher;
 	}
 }
-=======
-package cipher.pack;
 
-public class Cesar {
-	private int key=4;
-	
-	public String decrypt(String cipher) {		
-		String plaintext="";
-		int i=0;
-		while(i < cipher.length()) {
-			char c = cipher.charAt(i);
-			int asciCode = (int)c-this.key;
-			c=(char)asciCode;
-			plaintext+=c;
-			i++;
-		}
-		return plaintext;
-	}
-	
-	public String encrypt(String plaintext) {	
-		String cipher="";
-		int i=0;
-		while(i < plaintext.length()) {
-			char c = plaintext.charAt(i);
-			int asciCode = (int)c+this.key;
-			c=(char)asciCode;
-			cipher+=c;
-			i++;
-		}
-		return cipher;
-	}
-}
->>>>>>> origin/version-1.0.1
